4339d693c9edb85f0286e596a5d362c28a7a0c5c
Excigma <30280397+Excigma@users.noreply.github.com>
2023-08-03 13:54:03 +1200

    fix: doesn't work when your message isn't the last message in the channel

--- slack.com/up-to-edit.user.js
+++ slack.com/up-to-edit.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Up arrow to edit - Slack.com
-// @description  Up arrow will edit the previous message you sent in the channel, just like in Discord
+// @description  Pressing the up arrow will edit the previous message in the channel if you authored it
 // @author       Excigma
 // @namespace    https://excigma.xyz
 // @license      GPL-3.0
@@ -8,28 +8,22 @@
 // @match        https://app.slack.com/client/*
 // ==/UserScript==
 
+// This doesn't work when your message is not the last message in the channel
+
 // 'We have jquery at home'
 const $ = (...args) => document.querySelector.apply(document, args);
 
-// Shows user's name when hovering over the icon
-const PROFILE_SELECTOR = ".p-ia__nav__user__button"
 // Finds the last occurrence of the user's name in the nametag of a message
-const MESSAGE_SELECTOR = ".c-message_list .c-virtual_list__scroll_container > .c-virtual_list__item:last-of-type";
-
-// Remove prepended text
-const MENU_STRING = "User menu: ";
+const MESSAGE_SELECTOR = ".c-message_list .c-virtual_list__scroll_container > .c-virtual_list__item.c-virtual_list__item--initial-activeitem";
 
 document.addEventListener('keyup', (evt) => {
 	if (evt.key !== 'ArrowUp') return;
 
-	// Get the last message that the user sent into the channel by searching for the nametag
-	const name = $(PROFILE_SELECTOR).ariaLabel.replaceAll(MENU_STRING, "");
-	const lastMessage = $(`${MESSAGE_SELECTOR} [data-stringify-text="${name}"]`)?.closest(MESSAGE_SELECTOR);
-
-	if (!lastMessage) return;
+	const previousMessage = $(MESSAGE_SELECTOR);
+	if (!previousMessage) return;
 
 	// Dispatch "e" event to previous message to trigger edit
-	lastMessage.dispatchEvent(new KeyboardEvent('keydown', {
+	previousMessage.dispatchEvent(new KeyboardEvent('keydown', {
 		"key": "e",
 		"keyCode": 69
 	}));
