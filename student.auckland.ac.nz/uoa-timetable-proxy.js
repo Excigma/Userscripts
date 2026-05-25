56edee21a00a388a9336222ca6c88c698c0813ce
BirdMakingStuff <jiaxinyan@outlook.co.nz>
2026-05-25 20:44:19 +1200

    Read course code from event title instead of description (#7)
    Co-authored-by: Excigma <30280397+Excigma@users.noreply.github.com>

--- student.auckland.ac.nz/uoa-timetable-proxy.js
+++ student.auckland.ac.nz/uoa-timetable-proxy.js
@@ -57,10 +57,24 @@ I haven't tested this yet. No clue if it actually works.
 		// Matches an event in the iCalendar file.
 		return icsString.replace(/BEGIN:VEVENT([\s\S]*?)END:VEVENT/gm, (event) => {
 			const description = event.match(this.buildFieldRegex("DESCRIPTION"))?.[1];
+			const prevSummary = event.match(this.buildFieldRegex("SUMMARY"))?.[1];
 			const parsed = this.parseDescription(description);
 
 			// Can't find the description field, return the event as is
 			if (description === undefined) return event;
+
+			// Extract module code from SUMMARY (everything before the first /)
+
+			if (prevSummary) {
+				// Some courses have multiple course codes. We'll just use the first one here.
+				// Sometimes the number is the same, but the subject is different
+				// Sometimes the subject and number are both different
+				// It's not really worth the space to use it up. If someone does get annoyed, then I can look into it. IMO being able to see the number is more important than the subject.
+				const moduleCode = prevSummary.split("/")[0].trim();
+				if (/\b[A-Z]+\s\d{3}[A-Z]?\b/.test(moduleCode)) {
+					parsed.set("moduleusertext2", moduleCode);
+				}
+			}
 
 			// Keys from the DESCRIPTION to be used to replace the SUMMARY/LOCATION field
 			const summaryTemplate = this.defaultSummaryTemplate;
